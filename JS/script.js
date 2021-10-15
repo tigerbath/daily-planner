0f3dbd1cb90b0745bd19731326c0c9d4ff663f8c
Matthew Palmer <mattjpalmer92@gmail.com>
2021-10-15 17:14:39 +0100

    declared on load event and added event listener

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,13 @@
-// header day date and time using moment.js
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+// // const getDateAndTime = document.getElementById("date");
+
+// const getDateAndTime = () => {
+//   let dateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
+//   dateElement.innerHTML = dateAndTime;
+// };
+
+// declare on load event handler function
+const onLoad = function () {
+  console.log("bob");
+};
+// add on load event listener
+$(document).ready(onLoad);
