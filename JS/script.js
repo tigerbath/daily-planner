eb9dba5100189aa04de785a03d387bf59a1ba560
Matthew Palmer <mattjpalmer92@gmail.com>
2021-10-15 17:26:11 +0100

    added global declerations

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,48 @@
 //   let dateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
 //   dateElement.innerHTML = dateAndTime;
 // };
+// add global declarations
+const timeBlockLabels = [
+  {
+    timeBlockLabel: "09:00",
+    localStorageKey: 9,
+  },
+  {
+    timeBlockLabel: "10:00",
+    localStorageKey: 10,
+  },
+  {
+    timeBlockLabel: "11:00",
+    localStorageKey: 11,
+  },
+  {
+    timeBlockLabel: "12:00",
+    localStorageKey: 12,
+  },
+  {
+    timeBlockLabel: "13:00",
+    localStorageKey: 13,
+  },
+  {
+    timeBlockLabel: "14:00",
+    localStorageKey: 14,
+  },
+  {
+    timeBlockLabel: "15:00",
+    localStorageKey: 15,
+  },
+  {
+    timeBlockLabel: "16:00",
+    localStorageKey: 16,
+  },
+  {
+    timeBlockLabel: "17:00",
+    localStorageKey: 17,
+  },
+];
 
+// declare render timeblocks
+const renderTimeblocks = function () {};
 // declare on load event handler function
 const onLoad = function () {
   console.log("bob");
